24958fc45d77389a9e0466d38d2a42b422c79024
Andrés Fernández <andresavena19@gmail.com>
2021-12-21 13:28:09 -0300

    feat: added mapping boilerplate

---
+++ src/mappings/index.ts
@@ -0,0 +1,11 @@
+import {
+	Approval,
+	ApprovalForAll,
+	Transfer
+} from "../../generated/artblocks/joyWorld";
+
+export function handleApproval(event: Approval): void { }
+
+export function handleApprovalForAll(event: ApprovalForAll): void { }
+
+export function handleTransfer(event: Transfer): void { }
