2c156e93509bb50c2dc16ce8621f808392b075ea
Andrés Fernández <andresavena19@gmail.com>
2021-12-23 12:43:49 -0300

    feat: added metadata library

---
+++ src/modules/metadata/index.ts
@@ -0,0 +1,36 @@
+import { BigInt, Bytes } from "@graphprotocol/graph-ts"
+import { Block, Transaction } from "../../../generated/schema"
+
+export namespace metadata {
+	export namespace transactions {
+		export function getOrCreateTransactionMeta(
+			id: string, blockId: string, hash: Bytes,
+			from: Bytes, gasLimit: BigInt, gasPrice: BigInt
+		): Transaction {
+			let meta = Transaction.load(id)
+			if (meta == null) {
+				meta = new Transaction(id)
+				meta.block = blockId
+				meta.hash = hash
+				meta.from = from
+				meta.gasLimit = gasLimit
+				meta.gasPrice = gasPrice
+			}
+			return meta as Transaction
+		}
+	}
+
+	export namespace blocks {
+		export function getOrCreateBlock(
+			id: string, timestamp: BigInt, number: BigInt
+		): Block {
+			let block = Block.load(id)
+			if (block == null) {
+				block = new Block(id)
+				block.timestamp = timestamp
+				block.number = number
+			}
+			return block as Block
+		}
+	}
+}
